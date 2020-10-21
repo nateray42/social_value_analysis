cd6a415565a9123456bd32d429808113d4dff919
nateray42 <58092114+nateray42@users.noreply.github.com>
2020-10-21 17:00:28 +0900

    Stopwords added
    And minor changes

--- Preprocessing/Preprocessing.py
+++ Preprocessing/Preprocessing.py
@@ -2,31 +2,47 @@
 # coding: utf-8
 # %%
 import sys
-
+import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS
-from eunjeon import Mecab
+from konlpy.tag import Okt
 from collections import Counter
 
 
 # %%
-df = pd.read_csv("../_Data/2020_10_19[기업 사회적 가치].csv")
-
-text = df['Article'].str.cat(sep=' ')
+def df_to_text(df):
+    text = df['Article'].str.cat(sep=' ')
+    return text
 
 
 # %%
-def get_noun(text):
+def tokenize(df):
+    okt = Okt()
+    tokenized_data = []
+    #아래에 stopwords 추가 가능
+    stopwords = ['기자', '한겨레','의','가','이','은','들','는','좀','잘','걍','과','도','을', '를','으로','자','에','와','한','하다'] 
+    for sentence in df['Article']:
+        temp_X = okt.morphs(sentence, stem=True)
+        temp_X = [word for word in temp_X if not word in stopwords]
+        tokenized_data.append(temp_X)
+    
+    return tokenized_data
+    
 
-    mecab = Mecab()
-    noun = mecab.nouns(text)
+
+# %%
+def get_noun(df):
+    
+    okt = Okt()
+    text = df_to_text(df)
+    noun = okt.nouns(text)
     for i,v in enumerate(noun):
         if len(v) < 2:
             noun.pop(i)
 
     count = Counter(noun)
-    noun_list = count.most_common(100)
+    noun_list = count.most_common(70)
 
     return noun_list
 
@@ -42,12 +58,47 @@ def visualize(noun_list):
         max_font_size=300)
 
     wc.generate_from_frequencies(dict(noun_list))
-    wc.to_file('keyword.png')
+    return wc
 
 
 # %%
-noun_list = get_noun(text)
-visualize(noun_list)
+def wc_to_file(wc):
+    dt = datetime.datetime.now()
+    date = dt.strftime('%Y_%m_%d-%H%M%S')
+    wc.to_file('../_Outputs/' + 'wc_[' + date + '].png')
 
+
+# %%
+def text_to_file(text):
+    dt = datetime.datetime.now()
+    date = dt.strftime('%Y_%m_%d-%H%M%S')
+    f = open('../_Data/' + date + '.txt', "w", -1, 'utf-8')
+    f.write(str(text))
+    f.close()
+
+
+# %%
+df = pd.read_csv("../_Data/2020_10_21[기업 사회].csv")
+
+# %%
+noun_list = get_noun(df)
+wc = visualize(noun_list)
+
+
+# %%
+wc_to_file(wc)
+
+# %%
+text = df_to_text(df)
+text_to_file(text)
+
+# %%
+df['Article'] = df['Article'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+
+# %%
+tokens = tokenize(df)
+
+# %%
+text_to_file(tokens)
 
 # %%
