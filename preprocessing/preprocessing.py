d3e5fad698a465200bb7f956904b1b9d34f2c4f5
nateray42 <58092114+nateray42@users.noreply.github.com>
2020-10-26 19:14:07 +0900

    added dataframe concat

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -5,38 +5,43 @@ import sys
 import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
-from wordcloud import WordCloud, STOPWORDS
-from konlpy.tag import Okt
+from wordcloud import WordCloud
+from eunjeon import Mecab
 from collections import Counter
 
 
 # %%
+# def df_concat(df_1, df_2):
+#     df = pd.concat()
+#     df.drop_duplicates(['Title'], keep='first', inplace = True, ignore_index = True)
+#     return df
+
+# %%
 def df_to_text(df):
-    text = df['Article'].str.cat(sep=' ')
+    text = df['article'].str.cat(sep=' ')
     return text
 
 
 # %%
 def tokenize(df):
-    okt = Okt()
+    mecab = Mecab()
     tokenized_data = []
     #아래에 stopwords 추가 가능
-    stopwords = ['기자', '한겨레','의','가','이','은','들','는','좀','잘','걍','과','도','을', '를','으로','자','에','와','한','하다'] 
-    for sentence in df['Article']:
-        temp_X = okt.morphs(sentence, stem=True)
+    stopwords = ['기자', '한겨레'] 
+    for sentence in df['article']:
+        temp_X = mecab.nouns(sentence)
         temp_X = [word for word in temp_X if not word in stopwords]
         tokenized_data.append(temp_X)
     
     return tokenized_data
-    
 
 
 # %%
 def get_noun(df):
     
-    okt = Okt()
+    mecab = Mecab()
     text = df_to_text(df)
-    noun = okt.nouns(text)
+    noun = mecab.nouns(text)
     for i,v in enumerate(noun):
         if len(v) < 2:
             noun.pop(i)
@@ -65,7 +70,7 @@ def visualize(noun_list):
 def wc_to_file(wc):
     dt = datetime.datetime.now()
     date = dt.strftime('%Y_%m_%d-%H%M%S')
-    wc.to_file('../_Outputs/' + 'wc_[' + date + '].png')
+    wc.to_file('../_output/' + 'wc_[' + date + '].png')
 
 
 # %%
@@ -78,7 +83,24 @@ def text_to_file(text):
 
 
 # %%
-df = pd.read_csv("../_Data/2020_10_21[기업 사회].csv")
+df1 = pd.read_csv("../_Data/[중앙]2020_10_26_183435[기업 사회].csv")
+df2 = pd.read_csv("../_Data/[한경]2020_10_26_184732[기업 사회].csv")
+df3 = pd.read_csv("../_Data/[한겨레]2020_10_26_185605[기업 사회].csv")
+
+# %%
+df = pd.concat([df1, df2], ignore_index = True)
+
+# %%
+df = pd.concat([df, df3], ignore_index = True)
+
+# %%
+df.drop(columns=['Unnamed: 0'], inplace = True)
+
+# %%
+df.drop_duplicates(subset=['title'], inplace = True)
+
+# %%
+df.dropna(inplace = True)
 
 # %%
 noun_list = get_noun(df)
@@ -89,16 +111,10 @@ wc = visualize(noun_list)
 wc_to_file(wc)
 
 # %%
-text = df_to_text(df)
-text_to_file(text)
-
-# %%
-df['Article'] = df['Article'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+df['article'] = df['article'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
 
 # %%
 tokens = tokenize(df)
 
 # %%
 text_to_file(tokens)
-
-# %%
